0361b84912d6d459c3ae5dd796100c5249a5dbfd
Kasim Matthews <n01419394@unf.edu>
2023-09-25 19:40:49 -0400

    extra work needs to be applied

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,56 @@
-
+import React from "react";
 import './App.css';
+import Axios from 'axios';
+
 
 function App() {
+  function handleSubmit(e){
+    e.preventDefault();
+    let currentDate = new Date();
+
+
+    let partner = document.getElementById("partner");
+    let date = document.getElementById("date");
+    let source = document.getElementById("source");
+    let totalitems = document.getElementById("total-items");
+    let value = document.getElementById("value");
+    let deliverymethod = document.querySelector('input[name="delivery-method"]:checked').value;
+    let comments = document.getElementById("comments");
+
+    let givenDate = new Date(date);
+    let state = givenDate.getTime() < currentDate.getTime();
+    
+
+    Axios.post("http://localhost:3001/api/insert", {partner:partner, date:date, source:source, totalitems: totalitems, value: value, deliverymethod: deliverymethod, comments: comments, state: state})
+  }
   return (
-    <div className="App">
-      Hello
-    </div>
+    <form id="distribution" method="POST" onSubmit={handleSubmit}>
+      <label for="partner">Partner</label>
+      <input type="text" id="partner" required/>
+      
+      <label for="date">Date</label>
+      <input type="date" id="date" min="2023-09-01" required/>
+
+      <label for="source">Source</label>
+      <input type="text" id="source" required/>
+
+      <label for="total-items">Total-Items</label>
+      <input type="number" id="total-items" required/>
+
+      <label for="value">Value</label>
+      <input type="number" id="value" step=".01"/>
+
+      <p>Delivery Method</p>
+
+      <label for="pickup">Pickup</label>
+      <input type="radio" id="pickup" name="delivery-method" value="Pickup"/>
+      <label for="delivery">Delivery</label>
+      <input type="radio" id="delivery" name="delivery-method" value="Delivery"/>
+
+      <textarea name="comments" rows="4" cols="50">Comments</textarea>
+
+      <input type="submit" value="Submit"/>
+    </form>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const mysql = require('mysql')
 
 const db = mysql.createPool({
@@ -8,11 +10,32 @@ const db = mysql.createPool({
     password: 'WebVoyage2023!',
     database: 'test-db',
 });
+app.use(cors);
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.post('/api/insert', (req, res) =>{
 
-app.get('/', (req, res) =>{
-    res.send('hello world');
+    let partner = req.body.partner;
+    let date = req.body.date;
+    let source = req.body.source;
+    let totalitems = req.body.totalitems;
+    let value = req.body.value;
+    let deliverymethod = req.body.deliverymethod;
+    let comments = req.body.comments;
+    let state = req.body.state;
+
+    const sqlInsert = "INSERT INTO distributions (partner, date, source, total items, value, delivery method, comments, state) VALUES(?,?,?,?,?,?,?,?);"
+    db.query(sqlInsert, [partner, date, source, totalitems, value, deliverymethod, comments, state], (err, result) =>{
+        console.log(err);
+    })
 })
+
+/*app.get('/', (req, res) =>{
+    const sqlInsert = "INSERT INTO distributions (partner, date, source, total items, value, delivery method, comments, state) VALUES(?,?,?,?,?,?,?,?);"
+    db.query();
+    res.send('hello world');
+})*/
 
 app.listen('3001', () => {
     console.log("running on port 3001");
