09a728087ba7c71fb39f6b5dc5b60e3c07db2bbe
Kasim Matthews <n01419394@unf.edu>
2023-10-23 19:51:27 -0400

    omg I did it, claire you should have been here

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Distribution from './Distribution';
 
-ReactDOM.render(<Distribution />, document.getElementById("root"));
+export class Application extends React.Component{
+    render(){
+        return(
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />}/>
+                    <Route path="/distribution" element={<Distribution />} />
+                </Routes>
+            </BrowserRouter>
+        );
+    }
+}
+
+ReactDOM.render(<Application />, document.getElementById("root"));
 
 
