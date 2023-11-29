cdf86f2a9b0117d130363aed28314e279f53ce4a
ilykeegan <castrick1@gmail.com>
2023-11-28 19:11:24 -0500

    converting back to claire

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Distribution from './Distribution';
+
+
+
 
 export class Application extends React.Component{
-    render(){
-        return(
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<App />}/>
-                    <Route path="/distribution" element={<Distribution />} />
-                </Routes>
-            </BrowserRouter>
-        );
-    }
+
 }
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+
 
 
--- server/index.js
+++ server/index.js
@@ -1,42 +1,8 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const bodyParser = require('body-parser')
 const mysql = require('mysql2')
 
-const sb = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "WebVoyage2023!",
-    port: 3006
-});
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true}));
-
-app.get('/api/get', (req, res) =>{
-    const sqlGet = "SELECT * FROM test.dis;"
-    sb.query(sqlGet, (err, result) =>{
-        res.send(result);
-    }) 
-})
-
-app.post('/api/insert', (req, res) =>{
-
-    let partner = req.body.partner;
-    let date = req.body.date;
-    let source = req.body.source;
-    let totalitems = req.body.totalitems;
-    let value = req.body.value;
-    let deliverymethod = req.body.deliverymethod;
-    let comments = req.body.comments;
-    let state = req.body.state;
-
-    const sqlInsert = "INSERT INTO test.dis (partner, date, source, totalitems, value, deliverymethod, comments, state) VALUES (?,?,?,?,?,?,?,?);"
-    sb.query(sqlInsert, [partner, date, source, totalitems, value, deliverymethod, comments, state], (err, result) =>{
-        console.log(err);
-    })
-})
 
 const db = mysql.createPool({
     host: "localhost",
@@ -90,6 +56,6 @@ app.get('/', (req, res) =>{
     res.send('hello world');
 })
 
-app.listen('3000', () => {
-    console.log("running on port 3000");
+app.listen('3001', () => {
+    console.log("running on port 3001");
 })
