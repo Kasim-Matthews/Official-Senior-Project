edd9371a7d980975bbf6be3b9de4c6508da3b828
Kasim-Matthews <137832997+Kasim-Matthews@users.noreply.github.com>
2023-11-29 19:23:31 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -90,6 +90,6 @@ app.get('/', (req, res) =>{
     res.send('hello world');
 })
 
-app.listen('3001', () => {
-    console.log("running on port 3001");
+app.listen('3000', () => {
+    console.log("running on port 3000");
 })
