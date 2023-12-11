d4cf7bac124df7b60981e408c9fb9823a771d89a
Kasim Matthews <n01419394@unf.edu>
2023-12-11 14:18:37 -0500

    changes for cloud

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const register = () => {
-    Axios.post('http://localhost:3001/register', {
+    Axios.post('https://diaper-bank-inventory-management-system.onrender.com/register', {
       username: usernameReg, 
       password: passwordReg,
     }).then((response) => {
@@ -25,7 +25,7 @@ function App() {
   }
 
   const login = () => {
-    Axios.post('http://localhost:3001/login', {
+    Axios.post('https://diaper-bank-inventory-management-system.onrender.com/login', {
       username: username, 
       password: password,
     }).then((response) => {
