0f979ece500561a7d38e6805f4ded41355f346a2
Kasim Matthews <n01419394@unf.edu>
2024-04-21 17:44:42 -0400

    force error

--- server/controllers/vendorController.js
+++ server/controllers/vendorController.js
@@ -150,7 +150,7 @@ const vendor_create = async (req, res) => {
     }
 
     try {
-        const sqlInsert = `INSERT INTO public.partner ("Name", "Email", "PhoneNumber", "ContactName", "Type_id") VALUES ('{${Name}}', '{${Email}}', '{${Phone}}', '{${Contact}}', (SELECT partnertype."PartnerType_id" from public.partnertype WHERE "Type" = 'Vendor'))`
+        const sqlInsert = `INSERT INTO public.partner ("Name", "Email", "PhoneNumber", "ContactName", "Type") VALUES ('{${Name}}', '{${Email}}', '{${Phone}}', '{${Contact}}', (SELECT partnertype."PartnerType_id" from public.partnertype WHERE "Type" = 'Vendor'))`
         const response = await sb.query(sqlInsert)
         res.sendStatus(200)
         res.end();
