ddf208a9af45f47ce0447340b7e8f0c5abf8323f
Kasim Matthews <n01419394@unf.edu>
2024-04-22 20:40:45 -0400

    uhhh

--- client/src/OneManufacturer.js
+++ client/src/OneManufacturer.js
@@ -79,7 +79,7 @@ function ViewManufacturer() {
                 <table>
                     <thead>
                         <tr>
-                            <h3>Past Donations from {record.Manufacturer[0]}</h3>
+                            <h3>Past Donations from {record.Manufacturer.toString()}</h3>
                         </tr>
                         <tr>
                             <th>Date</th>
