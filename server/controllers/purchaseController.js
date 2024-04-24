929811b1e0e4badae5dc812b865ef7fa1ed5063a
Kasim Matthews <n01419394@unf.edu>
2024-04-23 21:01:47 -0400

    work now?

--- server/controllers/purchaseController.js
+++ server/controllers/purchaseController.js
@@ -109,9 +109,9 @@ const create = async (req, res) => {
         let results = itemlocations.rows
         let rows = [results]
         console.log(rows)
-        // for (let i= 0; i < Items.lenght; i++) {
-        //     rows.push(results[i].ItemLocation_id, results[i].Item_id, results[i].Location_id, results[i].Quantity + Items[i].Quantity)
-        // }
+        for (let i= 0; i < Items.lenght; i++) {
+            rows[i] = [results[i].ItemLocation_id, results[i].Item_id, results[i].Location_id, results[i].Quantity + Items[i].Quantity]
+        }
         // const updatelocations = `INSERT INTO public.itemlocation ("ItemLocation_id", "Item_id", "Location_id", "Quantity")
         // VALUES ${rows}
         // ON CONFLICT ("ItemLocation_id") DO UPDATE
