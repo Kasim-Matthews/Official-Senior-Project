38eb45b0d906cbab064bc0ec9b7c981b5863c65d
Kasim Matthews <n01419394@unf.edu>
2024-04-23 20:42:55 -0400

    lets just try it

--- server/controllers/purchaseController.js
+++ server/controllers/purchaseController.js
@@ -107,12 +107,16 @@ const create = async (req, res) => {
         const getitemlocations = `SELECT "ItemLocation_id", "Item_id", "Location_id", "Quantity" from public.itemlocation WHERE "Item_id" IN (${ids}) AND "Location_id" = ${Location}`
         const itemlocations = await sb.query(getitemlocations)
         let results = itemlocations.rows
-        console.log(results[0])
         let rows = []
         for (let i= 0; i < Items.lenght; i++) {
             rows.push(results[i].ItemLocation_id, results[i].Item_id, results[i].Location_id, results[i].Quantity - Items[i].Quantity)
         }
-        console.log(rows)
+        const updatelocations = `INSERT INTO public.itemlocation ("ItemLocation_id", "Item_id", "Location_id", "Quantity")
+        VALUES ${rows}
+        ON CONFLICT ("ItemLocation_id") DO UPDATE
+        SET "Quantity" = excluded."Quantity"`
+        const locationsupdated = await sb.query(updatelocations)
+        console.log(locationsupdated)
         res.sendStatus(200)
         res.end();
         return;
