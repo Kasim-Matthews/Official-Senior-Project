86e9f442303c177aa8d331b384624e396045ca8a
Kasim Matthews <n01419394@unf.edu>
2024-04-23 22:34:27 -0400

    giving in

--- server/controllers/purchaseController.js
+++ server/controllers/purchaseController.js
@@ -111,15 +111,15 @@ const create = async (req, res) => {
         for (let i = 0; i < Items.length; i++) {
             rows[i] = [results[i].ItemLocation_id, results[i].Item_id, results[i].Location_id, results[i].Quantity + parseInt(Items[i].Quantity)]
         }
-        console.log(rows.toString())
         console.log(rows)
-        console.log(rows.toLocaleString())
-        const updatelocations = `INSERT INTO public.itemlocation ("ItemLocation_id", "Item_id", "Location_id", "Quantity")
-        ${rows}
-        ON CONFLICT ("ItemLocation_id") DO UPDATE
-        SET "Quantity" = excluded."Quantity"`
-        const locationsupdated = await sb.query(updatelocations)
-        
+
+        for (let i = 0; i < Items.length; i++) {
+            const updatelocations = `INSERT INTO public.itemlocation ("ItemLocation_id", "Item_id", "Location_id", "Quantity")
+            (${rows[i]})
+            ON CONFLICT ("ItemLocation_id") DO UPDATE
+            SET "Quantity" = excluded."Quantity"`
+            const locationsupdated = await sb.query(updatelocations)
+        }
         res.sendStatus(200)
         res.end();
         return;
