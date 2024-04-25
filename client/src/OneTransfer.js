9334eac78f5ded91f9abbe47191d5f3e2e5adfd6
Kasim-Matthews <kasim14.km@gmail.com>
2024-04-25 17:03:21 -0400

    omg figured it out

--- client/src/OneTransfer.js
+++ client/src/OneTransfer.js
@@ -53,7 +53,7 @@ function ViewTransfer() {
             <table>
                 <thead>
                     <tr>
-                        <h3>Transfer from {info.Taken} to {info.Given} on {info.Date}</h3>
+                        <h3>Transfer from {info.Taken} to {info.Given} on {new Date(info.Date).toLocaleDateString()}</h3>
                     </tr>
                     <tr>
                         <th>Item</th>
