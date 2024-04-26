bd30f089118b1b65961f3ad8c199730a74493c5f
Kasim-Matthews <kasim14.km@gmail.com>
2024-04-25 21:54:47 -0400

    that one router thing was not it

--- server/routes/intake.js
+++ server/routes/intake.js
@@ -1,4 +1,8 @@
-const router = require('./router')
+const express = require("express");
+const router = express.Router();
+
+const cors = require('cors')
+router.use(cors())
 const intakeController = require('../controllers/intakeController')
 
 
