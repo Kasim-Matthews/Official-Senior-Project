de455bebbf2efe567287ba9c202506aa1bf450c4
Claire Strickland <castrick1@gmail.com>
2024-04-29 19:45:41 -0400

    update

--- client/src/login.js
+++ client/src/login.js
@@ -37,10 +37,6 @@ const Login = () => {
     }
             );
             console.log(JSON.stringify(response?.data));
-            //console.log(JSON.stringify(response));
-            const accessToken = response?.data?.accessToken;
-            const roles = response?.data?.roles;
-            setAuth({ user, pwd, roles, accessToken });
             setUser('');
             setPwd('');
             const redirectTo = from === "/" ? "/Dashboard" : from;
