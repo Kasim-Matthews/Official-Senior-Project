e1556b0f198fcc592d41ba0ff4a990f307a5f4ba
gelbels <n01474591@unf.edu>
2024-04-28 21:31:35 -0400

    select fixed

--- client/src/AddIntake.js
+++ client/src/AddIntake.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Axios from 'axios';
-import ItemInput from "./components/ItemInput";
-import DriveList from "./components/DriveList";
-import ManufacturerList from "./components/ManufacturerList";
-import DonationSiteList from "./components/DonationSiteList";
+import Navbar from "./components/navbar";
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import NativeSelect from '@mui/material/NativeSelect';
+
 
 function AddIntake() {
 
@@ -12,98 +16,18 @@ function AddIntake() {
     RecievedDate: "",
     Value: 0.00,
     Partner: 0,
-    Location: 0
+    location: 0,
+    item: 0,
+    Quantity: 0,
+    ItemLocationFK: 0
   })
 
-  function handleCancel() {
-    if (window.confirm("Are you sure you want to cancel") == true) {
-        window.location.href = "/intake";
-    }
-}
-
-
-  const [sourceType, setSourceType] = React.useState("")
+  const [partners, setPartners] = React.useState([])
+  const [items, setItems] = React.useState([])
   const [locations, setLocations] = React.useState([])
+  const [itemLocation, setItemLocation] = React.useState([])
 
-  const Types = ["Product Drive", "Donation Site", "Manufacturer", "Misc Donation"]
-
-  const [index, setIndex] = React.useState(1);
-  const [items, setItems] = React.useState([
-    {
-      name: `item [${index}]`,
-      Item_id: 0,
-      Quantity: 0,
-
-    }
-  ])
-
-  const [formErrors, setFormErrors] = useState({})
-
-  function listtype() {
-    if (sourceType == "Product Drive") {
-      return (
-        <DriveList handleChange={handleChange} />
-      )
-    }
-
-    else if (sourceType == "Manufacturer") {
-      return (
-        <ManufacturerList handleChange={handleChange} />
-      )
-    }
-
-    else if (sourceType == "Donation Site") {
-      return (
-        <DonationSiteList handleChange={handleChange} />
-      )
-    }
-  }
-
-  const handleItem = (e, index) => {
-    const values = [...items];
-    values[index].Item_id = e.target.value;
-    setItems(values)
-  }
-
-  const handleQuantity = (e, index) => {
-    const values = [...items];
-    values[index].Quantity = e.target.value;
-    setItems(values)
-  }
-
-  const handleAddField = (e) => {
-    e.preventDefault();
-    setIndex(index + 1);
-    const values = [...items];
-    if (values.length === 0) {
-      values.push(
-        {
-          name: `item [${0}]`,
-          Item_id: 0,
-          Quantity: 0,
-        }
-      );
-    }
-    else {
-      values.push(
-        {
-          name: `item [${index}]`,
-          Item_id: 0,
-          Quantity: 0,
-        }
-      );
-    }
-
-    setItems(values);
-  }
-
-  const handleDeleteField = (e, index) => {
-    const values = [...items];
-    values.splice(index, 1);
-    setItems(values);
-  };
-
-
+  
 
   function handleChange(event) {
     setFormData(prevFormData => {
@@ -114,114 +38,95 @@ function AddIntake() {
     })
   }
 
-  function sourceChange(event) {
-    setSourceType(event.target.value)
-    listtype(event.target.value)
-  }
-
-
-
   useEffect(() => {
-    Axios.get("http://localhost:3001/location/use").then((response) => {
-      setLocations(response.data);
+    Axios.get("http://localhost:3306/partner").then((response) => {
+      setPartners(response.data);
     })
   }, [])
 
- const typechecker = async (e) => {
-    e.preventDefault()
-    if (sourceType == "Misc Donation") {
-      await Axios.get("http://localhost:3001/intake/misc").then((response) => {
-        setFormData(prevFormData => {
-          return {
-            ...prevFormData,
-            Partner: response.data[0].Partner_id
-          }
+  useEffect(() => {
+    Axios.get("http://localhost:3306/item").then((response) =>{
+            setItems(response.data);
         })
-      })
-    }
-    validate();
-    return
-  }
+  }, [])
 
-  const validate = () => {
-    const errors = {};
-    const regex_comments = /^(?!.*SELECT|.*FROM|.*WHERE|.*UPDATE|.*INSERT).*$/;
+  useEffect(() => {
+    Axios.get("http://localhost:3306/location").then((response) =>{
+            setLocations(response.data);
+        })
+  }, [])
 
+  const submitPurchase = async (e) => {
+    e.preventDefault()
 
-    if (!regex_comments.test(formData.Comments)) {
-      errors.Comments = "The comments contains an SQL keyword !"
-    }
-    setFormErrors(errors)
-    if (!errors.Comments) {
-      submitDonation()
-    }
-    return;
-}
-  const submitDonation = async () => {
-    await Axios.post("http://localhost:3001/intake/new", { Comments: formData.Comments, RecievedDate: formData.RecievedDate, Partner: formData.Partner, Value: formData.Value })
+    Axios.post("http://localhost:3306/intake/new", {Comments: formData.Comments, RecievedDate: formData.RecievedDate, Value: formData.Value, Partner: formData.Partner })
+    
+    let IL_response = await Axios.post("http://localhost:3306/distribution/find_ild", {Item_id: formData.item, Location_id: formData.location})
 
-    let V_response = await Axios.post("http://localhost:3001/intake/find_value", { Items: items })
-    let IL_response = await Axios.post("http://localhost:3001/intake/location", { Items: items, Location_id: formData.Location })
+    let IID_response = await Axios.get("http://localhost:3306/intake/find_id");
 
-    await Axios.post("http://localhost:3001/intake/track", { Items: items, Values: V_response.data, FKItemLocation: IL_response.data });
-    await Axios.put("http://localhost:3001/intake/update_item", { Items: items, ItemLocationFK: IL_response.data });
+    Axios.post("http://localhost:3306/intake/track", {Intake_id: IID_response.data[0].Intake_id, Quantity: formData.Quantity, Value: formData.Value, FKItemLocation: IL_response.data[0].ItemLocation_id});
+
+    let current = await Axios.post("http://localhost:3306/intake/find_q", {ItemLocationFK: IL_response.data[0].ItemLocation_id})
+
+    Axios.put("http://localhost:3306/intake/update_item", {Quantity: formData.Quantity, ItemLocationFK: IL_response.data[0].ItemLocation_id, CurrentQ: current.data[0].Quantity});
+
     window.location.href = "/intake";
   }
 
-
   return (
     <div>
+      <Navbar /> 
       <h2>Intake</h2>
-      <form id="intake" onSubmit={typechecker}>
-
-        <label htmlFor="Source">Source</label>
-        <select id="Source" value={sourceType} onChange={sourceChange}>
-          <option value="" disabled></option>
-          {Types.map((type) => {
+      <form id="intake" onSubmit={submitPurchase}>
+      <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="Partner">Partner</InputLabel>
+        <NativeSelect>
+        {partners.map((val) => {
+          return (
+            <option value={val.Partner_id}>{val.Name}</option>
+          )
+        })}
+        </NativeSelect>
+      </FormControl>
+    </Box>
+        <TextField select defaultValue="Partner" helperText="Please select a partner" id="Partner" label="Partner" variant="outlined" value={formData.Partner} onChange={handleChange}/>
+          {partners.map((val) => {
             return (
-              <option value={type}>{type}</option>
+              <option value={val.Partner_id}>{val.Name}</option>
             )
           })}
-        </select>
-        <br />
-        {sourceType != "" ? listtype() : null}
+        <br></br>
 
-        <label htmlFor="Location">Location</label>
-        <select id="Location" name="Location" value={formData.Location} onChange={handleChange}>
-          <option value="">--Please choose an option--</option>
-          {locations.map((val) => {
-            return (
-              <option value={val.Location_id}>{val.Name}</option>
-            )
-          })}
-
-        </select><br />
-
-        <label htmlFor="RecievedDate">Issued on</label>
+        <label htmlFor="RecievedDate">Recieved Date</label>
         <input type="date" name="RecievedDate" id="RecievedDate" min="2023-09-01" value={formData.RecievedDate} onChange={handleChange} /><br></br>
 
-        <label htmlFor="Value">Money Raised</label>
-        <input type="number" name="Value" id="Value" step="0.01" value={formData.Value} onChange={handleChange} />
+        <label htmlFor="Value">Value</label>
+        <input type="number" name="Value" id="Value" step="0.01" value={formData.Value} onChange={handleChange}/>
         <textarea name="Comments" rows="4" cols="50" value={formData.Comments} onChange={handleChange} placeholder="Comment"></textarea><br></br>
-        {formErrors.Comments ? <p>{formErrors.Comments}</p> : null}
 
         <h2>Items</h2>
-        {items.map((obj, index) => (
-          <ItemInput
-            key={index}
-            objName={obj.name}
-            handleItem={handleItem}
-            handleQuantity={handleQuantity}
-            index={index}
-            deleteField={handleDeleteField}
-          />
-        ))}
-        <button name="add-btn" onClick={handleAddField}>
-          Add
-        </button>
+        <div style={{ display: "flex" }}>
+          <Textfield select defaultValue="Item Name" helperText="Please select an item" id="item" name="item" value={formData.item} onChange={handleChange}/>
+            {items.map((val) => {
+              return (
+                <option value={val.Item_id}>{val.Name}</option>
+              )
+            })}
 
-        <input type="submit" value="Submit" />
-        <button type="button" onClick={handleCancel}>Cancel</button>
+          <Textfield select defaultValue="Location" helperText="Please select a location" id="location" name="location" value={formData.location} onChange={handleChange}/>
+            {locations.map((val) => {
+              return (
+                <option value={val.Location_id}>{val.Name}</option>
+              )
+            })}
+
+          <input type="number" name="Quantity" id="Quantity" required onChange={handleChange} value={formData.Quantity} />
+        </div>
+
+        <Button variant="contained" type="submit" value="Submit" />
+
       </form>
     </div>
 
