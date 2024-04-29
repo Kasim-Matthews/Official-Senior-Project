5a751db181523bb46bd980c31800ef8ed09161cb
Claire Strickland <castrick1@gmail.com>
2024-04-29 19:38:23 -0400

    update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -29,32 +29,32 @@ const login = async (req, res) => {
         if (!foundUser) return res.sendStatus(401); // Unauthorized
 
         const match = await bcrypt.compare(pwd, foundUser.Password); // Ensure the column name matches your DB schema
-        if (match) {
-            const roles = Object.values(foundUser.Role);
-            // Here you would create JWTs or perform other login success actions
-            const accessToken = jwt.sign(
-                { 
-                    "UserInfo": {
-                        "username": foundUser.Username,
-                        "roles": roles
-                }
-            },
-                process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '10m' }
-            );
-            const refreshToken = jwt.sign(
-                { "username": foundUser.Username },
-                process.env.REFRESH_TOKEN_SECRET,
-                { expiresIn: '1d' }
-            );
+        // if (match) {
+        //     const roles = Object.values(foundUser.Role);
+        //     // Here you would create JWTs or perform other login success actions
+        //     const accessToken = jwt.sign(
+        //         { 
+        //             "UserInfo": {
+        //                 "username": foundUser.Username,
+        //                 "roles": roles
+        //         }
+        //     },
+        //         process.env.ACCESS_TOKEN_SECRET,
+        //         { expiresIn: '10m' }
+        //     );
+        //     const refreshToken = jwt.sign(
+        //         { "username": foundUser.Username },
+        //         process.env.REFRESH_TOKEN_SECRET,
+        //         { expiresIn: '1d' }
+        //     );
 
-            await sb.query(`UPDATE public.user SET "RefreshToken" = '${refreshToken}' WHERE "User_id" = ${foundUser.User_id}`);
+        //     await sb.query(`UPDATE public.user SET "RefreshToken" = '${refreshToken}' WHERE "User_id" = ${foundUser.User_id}`);
 
-            res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
-            res.json({ accessToken });
-        } else {
-            res.sendStatus(401); // Unauthorized
-        }
+        //     res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
+        //     res.json({ accessToken });
+        // } else {
+        //     res.sendStatus(401); // Unauthorized
+        // }
     } catch (err) {
         res.status(500).json({ 'message': err.message });
     }
