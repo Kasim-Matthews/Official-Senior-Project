77538d31a0402459f495c08f1f25d3b7ce7a4355
Kasim-Matthews <kasim14.km@gmail.com>
2024-05-01 20:08:59 -0400

    now I will really see wtf I gotta do

--- client/src/Intake.js
+++ client/src/Intake.js
@@ -71,7 +71,7 @@ function Intake() {
                 const response = await Axios.put(`${process.env.REACT_APP_BACKEND_URL}/donation/reclaim`, { id: id })
 
                 if(response.data.status == 409){
-                    console.log(response.data)
+                    console.log(response.data.data)
                     alert(`These items: ${response.data.data} quantities are lower than what you want to take away`)
                 }
 
